376864e472980554a6c5e3f6e3fba0a4b81a8ad3
Willsr71 <willsr71@gmail.com>
2016-03-18 18:12:48 -0400

    More stuff

--- compile.py
+++ compile.py
@@ -5,6 +5,7 @@ import difflib
 
 config = {}
 mods = []
+packaged_files = []
 
 def get_config():
     global config
@@ -32,28 +33,62 @@ def makedirs():
 def getmatchpercentage(string1, string2):
   return int(round(difflib.SequenceMatcher(None, string1, string2).ratio() * 100))
     
-def getmatch(file):
+def gettopmatches(file):
   matches = []
-  top_percentage = 0
+  top_percentages = [0]
+  
+  file = file.split(".jar")[0]
+  if not config["matching"]["case_sensitive"]:
+    file = file.lower()
   
   for mod in mods:
     percentage = getmatchpercentage(mod, file)
-    if percentage < 50: continue
+    if percentage < config["matching"]["percentage_required"]: continue
     
     matches.append({"percentage": str(percentage), "mod": str(mod)})
     
-    if (percentage > top_percentage):
-      top_percentage = percentage
+    for x in range(0, len(top_percentages)):
+      if percentage > top_percentages[x]:
+        top_percentages.add(percentage)
   
-  print(matches)
+  print("Top percentages", top_percentages)
+  print("Matches", matches)
+  top_matches = []
+  
   for match in matches:
-    if match["percentage"] == str(top_percentage):
-      return match["mod"]
+    if match["percentage"] in top_percentages:
+      top_matches.append(match["mod"])
+  
+  print("Top matches", top_matches)
+  return top_matches
     
-def package(file):
+def prepare_packages(file):
+  global packaged_files
+  
   print(file)
-  match = getmatch(file.lower().split(".jar")[0])
-  print("Match: " + match)
+  top_matches = gettopmatches(file)
+  print(top_matches)
+  
+  for x in range(0, 4):
+    print("Mod file:   " + file)
+    print("Best match: " + top_matches[x])
+    use = print("Use this match?")
+    if (use):
+      version = print("Version?")
+      packaged_files.append({"mod_file": file, "slug": top_matches[x], "version": version})
+      return
+  
+  slug = print("No matches found. Please enter mod slug.")
+  version = print("Version?")
+  packaged_files.append({"mod_file": file, "slug": slug, "version": version})
+
+def package_file(mod):
+  if not os.path.exists("solder/mods/" + mod["slug"]):
+    os.mkdir("solder/mods/" + mod["slug"])
+  
+  mod_file = mod["mod_file"]
+  packaged_file = mod["slug"] + "-" + mod["version"]
+  
 
 def main():
   get_config()
@@ -62,6 +97,8 @@ def main():
   makedirs()
   
   for file in os.listdir("additions/mods"):
-    package(file)
+    prepare_packages(file)
+  for mod in packaged_files:
+    package_file(mod)
   
 main()
