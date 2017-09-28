8a0d10eb5a8743727bb4f7d44c54ad70c9dc856e
Willsr71 <willsr71@gmail.com>
2017-09-28 16:00:55 -0400

    Fix formatting

--- compile.py
+++ compile.py
@@ -1,7 +1,8 @@
+import difflib
 import json
 import os
+
 import requests
-import difflib
 
 config = {}
 mods = []
@@ -49,13 +50,14 @@ def gettopmatches(file):
 
     for mod in mods:
         percentage = getmatchpercentage(mod, file)
-        if percentage < config["matching"]["percentage_required"]: continue
+        if percentage < config["matching"]["percentage_required"]:
+            continue
 
         matches.append({"percentage": str(percentage), "mod": str(mod)})
 
         for x in range(0, len(top_percentages)):
             if percentage > top_percentages[x]:
-                top_percentages.add(percentage)
+                top_percentages.append(percentage)
 
     print("Top percentages", top_percentages)
     print("Matches", matches)
@@ -79,9 +81,9 @@ def prepare_packages(file):
     for x in range(0, 4):
         print("Mod file:   " + file)
         print("Best match: " + top_matches[x])
-        use = print("Use this match?")
-        if (use):
-            version = print("Version?")
+        use = input("Use this match?").lower()
+        if use.startswith("y"):
+            version = input("Version?")
             packaged_files.append({"mod_file": file, "slug": top_matches[x], "version": version})
             return
 
